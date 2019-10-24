77c7151b5ad42865e0507b9f553963f187041274
Piersanti dylan <dylngae@outlook.com>
2019-10-24 13:34:17 +0200

    FirstCommit Install

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: "app",
+  /* computed: {
+    monNom () {
+      return this.test(4)
+    }
+  },
+  data () {
+    return {
+      message: "Test de VueJS avec BootStrap"
+    }
+  },
+  methods: {
+    test (num) {
+      return `Numuro: ${num}`
+    }
+  }, */
+  components: {
+    // HelloWorld
+  }
+};
+</script>
+
+<style>
+body {
+  background-color:red;
+}
+
+.navBar span {
+  color: #fff;
+  margin: 0 0 0 125px;
+}
+</style>
